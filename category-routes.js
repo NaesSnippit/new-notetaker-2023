96a1b26d06c6cd83550f7816fcefbca8c311e8c8
Nae <120901516+NaesSnippit@users.noreply.github.com>
2023-08-28 13:29:43 -0700

    Create category-routes.js

---
+++ category-routes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+// GET all categories
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET one category by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this ID' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST create new category
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// PUT update category by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!category[0]) {
+      res.status(404).json({ message: 'No category found with this ID' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE category by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const category = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this ID' });
+      return;
+    }
+    res.status(200).json({ message: 'Category deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
